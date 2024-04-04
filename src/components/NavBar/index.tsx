32fed38f5d33656fea0a4f1862d0ecea8255396a
Oday Ali <ody.ali.eng@gmail.com>
2024-04-04 18:50:00 +0300

    contact

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -7,7 +7,8 @@ const Navbar: React.FC<NavBarProps> = ({ tabs }: NavBarProps) => {
         <nav className="h-[60px] w-full flex flex-row items-center justify-center">
             {tabs.map(tab =>
             (<NavLink key={tab.name} to={tab.url}
-                className={({ isActive }) => isActive ?
+                className={({ isActive }) => isActive &&
+                    !(tab.name === 'Home' && window.location.pathname.split('/').length > 2) ?
                     "text-white text-bold bg-blue-400 hover:bg-blue-600 rounded-md px-2 py-1 mr-4 text-md font-semibold" :
                     "text-Blue-400 hover:bg-blue-100 hover:text-white rounded-md px-2 py-1 mr-4 text-md font-semibold"}>
                 {tab.name}
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,34 @@
-export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Page</h1>
+import React from 'react';
+import { FaMapMarkerAlt, FaEnvelope, FaPhone, FaLinkedin } from 'react-icons/fa';
+
+const Contact: React.FC = () => {
+  return (
+    <div className="bg-gray-100 p-8 rounded-lg">
+      <h2 className="text-2xl font-semibold mb-4">Contact Information</h2>
+
+      <div className="flex flex-col space-y-4">
+        <div className="flex items-center space-x-4">
+          <FaMapMarkerAlt className="text-gray-600" />
+          <span>{`${'Jumraya'}, ${'Damascus'}, ${'Syria'}`}</span>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <FaEnvelope className="text-gray-600" />
+          <a href={`mailto:${'ody.ali.eng@gmail.com'}`}>{'ody.ali.eng@gmail.com'}</a>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <FaPhone className="text-gray-600" />
+          <a href={`tel:${'+963-96451523'}`}>{'+963-96451523'}</a>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <FaLinkedin className="text-gray-600" />
+          <a href={'https://odayali2249.github.io/my-portfolio'} target="_blank" rel="noopener noreferrer">{'Linkedin'}</a>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default Contact;
